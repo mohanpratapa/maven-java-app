c678ceaccb0e25686335af715dc034a4be4dc559
mohanpratapa <pvskrishnamohan24@gmail.com>
2021-02-19 18:32:49 -0600

    added initial maven project

---
+++ src/main/java/com/pratapa/mohan/bigdata/App.java
@@ -0,0 +1,13 @@
+package com.pratapa.mohan.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
